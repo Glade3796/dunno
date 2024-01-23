d27c89224b467da5f7e3eaadf67c46895440a66f
Glade3796 <151785784+Glade3796@users.noreply.github.com>
2024-01-23 15:21:12 +0000

    init

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="b">
+      <h1 className="font-bold text-6xl">Dunno</h1>
+      <h3>List of dunnos:</h3>
+    </main>
+  );
+}
