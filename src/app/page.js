40407ba6be3c855bee1b238e62208c01f01d1d9f
Glade3796 <151785784+Glade3796@users.noreply.github.com>
2024-01-24 13:13:43 +0000

    ss

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,22 @@
-import Image from "next/image";
+import { sql } from "@vercel/postgres";
+import { Yesteryear } from "next/font/google";
 
-export default function Home() {
+export default async function Home() {
+  "use client";
+  const posts = await sql`
+    SELECT * FROM posts;
+  `;
+  console.log(posts.rows);
   return (
     <main className="b">
       <h1 className="font-bold text-6xl">Dunno</h1>
       <h3>List of dunnos:</h3>
+      <ul>
+        {posts.rows.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </main>
   );
 }
+
